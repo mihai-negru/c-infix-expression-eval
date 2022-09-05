80435c56726df01797caf6408471ce3ee3d4d5b2
Mihai Negru <determinant289@gmail.com>
2022-09-05 15:42:24 +0300

    Finished function to evaluate double postfix expr

--- src/evaluate_postfix.c
+++ src/evaluate_postfix.c
@@ -1,6 +1,4 @@
 #include "./include/expression_utils.h"
-#include <limits.h>
-#include <math.h>
 
 long long int evaluate_int_postfix(char *postfix_str) {
     if (postfix_str == NULL) {
@@ -150,6 +148,144 @@ long long int evaluate_int_postfix(char *postfix_str) {
     return result;
 }
 
+
+
+
+
 long double evaluate_double_postfix(char *postfix_str) {
-    return 0;
+    if (postfix_str == NULL) {
+        return LDBL_MAX;
+    }
+
+    stack_t *eval_stack = create_stack(NULL, sizeof(long double));
+
+    if (eval_stack == NULL) {
+        return LDBL_MAX;
+    }
+
+    char *save_token = NULL;
+    char *token = __strtok_r(postfix_str, WORD_DELIMITERS, &save_token);
+
+    scl_error_t err = SCL_OK;
+
+    uint8_t op_precedence = 0;
+    long double operand = LDBL_MAX;
+
+    while (token != NULL) {
+        if ((op_precedence = operator_precedence(token[0])) != 0) {
+            operand = LDBL_MAX;
+
+            if (op_precedence == 7) {
+                operand  = *(const long double *)stack_top(eval_stack);
+
+                err = stack_pop(eval_stack);
+                
+                if (err != SCL_OK) {
+                    free_stack(eval_stack);
+
+                    return LDBL_MAX;
+                }
+
+                switch (token[0]) {
+                    case 'P':
+                        operand = +operand;
+                        break;
+                    case 'M':
+                        operand = -operand;
+                        break;
+                    case 'N':
+                        fprintf(stderr, "Invalid operator for double : <~>\n");
+
+                        free_stack(eval_stack);
+
+                        return LDBL_MAX;
+                }
+            } else {
+                long double foperand = *(const long double *)stack_top(eval_stack);
+
+                err = stack_pop(eval_stack);
+                
+                if (err != SCL_OK) {
+                    free_stack(eval_stack);
+
+                    return LDBL_MAX;
+                }
+
+                long double soperand = *(const long double *)stack_top(eval_stack);
+
+                err = stack_pop(eval_stack);
+                
+                if (err != SCL_OK) {
+                    free_stack(eval_stack);
+
+                    return LDBL_MAX;
+                }
+
+                switch (token[0]) {
+                    case '^':
+                        operand = (long double)pow((double)soperand, (double)foperand);
+                        break;
+                    case '*':
+                        operand = (soperand * foperand);
+                        break;
+                    case '/':
+                        operand = (soperand / foperand);
+                        break;
+                    case '+':
+                        operand = (soperand + foperand);
+                        break;
+                    case '-':
+                        operand = (soperand - foperand);
+                        break;
+                    default:
+                        fprintf(stderr, "Invalid operator for double : <%c>\n", token[0]);
+                        free_stack(eval_stack);
+                        
+                        return LDBL_MAX;
+                }
+            }
+        } else {
+            operand = strtold(token, NULL);
+
+            if ((operand == LDBL_MAX) || (operand == LDBL_MIN)) {
+                fprintf(stderr, "Error: Invalid operand \"%s\"\n", token);
+
+                free_stack(eval_stack);
+
+                return LDBL_MAX;
+            }
+        }
+
+        err = stack_push(eval_stack, toptr(operand));
+
+        if (err != SCL_OK) {
+            free_stack(eval_stack);
+
+            return LDBL_MAX;
+        }
+
+        token = __strtok_r(NULL, WORD_DELIMITERS, &save_token);
+    }
+
+    long double result = *(const long double *)stack_top(eval_stack);
+
+    err = stack_pop(eval_stack);
+
+    if (err != SCL_OK) {
+        free_stack(eval_stack);
+
+        return LDBL_MAX;
+    }
+
+    if (!is_stack_empty(eval_stack)) {
+        fprintf(stderr, "Error: Invalid postfix expression\n");
+
+        free_stack(eval_stack);
+
+        return LDBL_MAX;
+    }
+
+    free_stack(eval_stack);
+
+    return result;
 }
